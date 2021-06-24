fd4b33659ce9f559b1bf95d23e42d9f7390ba899
nandani <nandanik2012@gmail.com>
2021-06-24 08:47:10 +0000

    abc,xyz done

---
+++ newo/main.tf
@@ -0,0 +1,14 @@
+provider{
+terraform
+}
+
+resource "vpc""vpc name"{
+  name="varvpc"
+ }
+resource ="ec2 instance vm1"{
+name = "vm2"
+}
+resource ="ec2 instance vm1"{
+name = "vm2"
+}
+
---
+++ newo/new.tf
@@ -0,0 +1,10 @@
+
+
+new = {
+<<<<<<< Updated upstream
+  abc
+}
+=======
+vbnvnbvn}
+>>>>>>> Stashed changes
+
